412f3268d3946fe6ecc6493a0fdf53908c6a40d8
ByteNate <nateclaussen@gmail.com>
2024-05-04 11:48:02 -0500

    modify database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect('mongodb://localhost:3000/tapairing', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
