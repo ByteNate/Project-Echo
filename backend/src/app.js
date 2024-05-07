d272ce208fec82e8d37680b07b80f250c91a41ad
ByteNate <nateclaussen@gmail.com>
2024-05-07 18:50:29 -0500

    change port

--- backend/src/app.js
+++ backend/src/app.js
@@ -57,7 +57,7 @@ app.use(errorLogger);
 app.use(errorHandler);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
